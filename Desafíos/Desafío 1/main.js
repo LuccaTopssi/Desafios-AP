bf9b83250b0d0f13233c27658ed46d85737c4df9
Imara Pérez Rao <91611508+LuccaTopssi@users.noreply.github.com>
2024-04-26 18:21:33 -0300

    Delete Desafíos/Desafío 1 directory

--- Desafíos/Desafío 1/main.js
+++
@@ -1,5 +0,0 @@
-let nombreProducto = prompt('¿Qué producto quieres comprar?')
-let precioUnitario = prompt('¿Cuánto sale cada uno de ese producto?')
-let cantidadDeseada = prompt('¿Cuánto quiere comprar de ese producto?')
-let precioFinal = parseInt(precioUnitario*cantidadDeseada)
-alert(`El costo final de ${nombreProducto} es de: $${precioFinal}`)
