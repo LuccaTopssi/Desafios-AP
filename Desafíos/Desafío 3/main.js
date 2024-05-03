177d851964cc37120fde617d262d1fceeb100c81
Imara Pérez Rao <luccaxd.lpr@gmail.com>
2024-05-03 18:39:17 -0300

    Cambios Finales desafio tres

--- Desafíos/Desafío 3/main.js
+++ Desafíos/Desafío 3/main.js
@@ -2,6 +2,9 @@ let nombreProducto = prompt('¿Qué producto quieres comprar?')
 let precioUnitario = prompt('¿Cuánto sale cada uno de ese producto?')
 let cantidadDeseada = prompt('¿Cuánto quiere comprar de ese producto?')
 precioTotal = parseInt(precioUnitario*cantidadDeseada)
-if(cantidadDeseada >= 5)
+if(cantidadDeseada >= 5) {
     precioFinal = parseInt(precioTotal - precioTotal * 10 / 100)
-alert(`El costo final de ${nombreProducto} es de: $${precioFinal}`)
+    alert(`El costo final con el descuento del 10% de ${nombreProducto} es de: $${precioFinal}`)
+}
+else(cantidadDeseada < 5)
+    alert(`El costo final de ${nombreProducto} es de: $${precioFinal}`)
